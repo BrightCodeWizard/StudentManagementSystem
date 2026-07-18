4be052e95dd612d2261698e6c661c276df38ad2c
Bright Odoom Dadson <obrightersunshine@gmail.com>
2026-07-18 03:43:00 +0000

    Implemented student list page

--- students/views.py
+++ students/views.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from .models import Student
 
-# Create your views here.
+
+@login_required
+def student_list(request):
+    students = Student.objects.all()
+
+    context = {
+        'students': students
+    }
+
+    return render(request, 'students/student_list.html', context)
